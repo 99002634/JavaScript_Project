d3cb13300f5897ff16a8e2b14ebf9bec597215c9
Harshavardhan C.A <71482239+99002634@users.noreply.github.com>
2020-11-10 12:14:42 +0530

    Update BankwebsiteSpec.js
    Jasmine framework for Business Loan Application

--- JavaScript_miniproject/3_Implementation/Jasmine Frame work/spec/BankwebsiteSpec.js
+++ JavaScript_miniproject/3_Implementation/Jasmine Frame work/spec/BankwebsiteSpec.js
@@ -1,1 +1,30 @@
-//Jasmine Framework
+describe('Testing the Business Loan Application', ()=>{
+    it("Check for Full Name", ()=>{
+        const res = fullnameTest("Harsha");
+        expect(res).toBeTruthy();
+    })
+    it("Check for Phone No.", ()=>{
+        const res = phonenoTest("7996485910");
+        expect(res).toBeTruthy();
+    })
+    it("Check for Address", ()=>{
+        const res = addressTest("Kolar");
+        expect(res).toBeTruthy();
+    })
+    it("Check for Branch", ()=>{
+        const res = branchTest("Kolar");
+        expect(res).toBeTruthy();
+    })
+    it("Check for Business Name", ()=>{
+        const res = businessnameTest("New Company");
+        expect(res).toBeTruthy();
+    })
+    it("Check for Amount", ()=>{
+        const res = amountTest("100000");
+        expect(res).toBeTruthy();
+    })
+    it("Check for Loan Tenure", ()=>{
+        const res = loantenureTest("5");
+        expect(res).toBeTruthy();
+    })
+});
