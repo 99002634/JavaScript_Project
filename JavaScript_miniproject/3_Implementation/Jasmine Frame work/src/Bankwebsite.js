f0797984144c1bdfed89e86d7022f703133b9182
Harshavardhan C.A <71482239+99002634@users.noreply.github.com>
2020-11-10 12:16:25 +0530

    Update Bankwebsite.js
    Business Loan Application

--- JavaScript_miniproject/3_Implementation/Jasmine Frame work/src/Bankwebsite.js
+++ JavaScript_miniproject/3_Implementation/Jasmine Frame work/src/Bankwebsite.js
@@ -1,1 +1,36 @@
-//Jasmine Framework
+//Business Loan Application
+function fullnameTest(fullname)
+{
+    let regex = /^[a-zA-Z ]{2,30}$/;
+    return regex.test(fullname);  
+}
+function phonenoTest(phone) 
+{
+    let regex = /^[(0/91)]?[7-9][0-9]{9}$/;
+    return regex.test(phone);
+}
+function addressTest(address)
+{
+    let regex = /^[a-zA-Z ]{2,30}$/;
+    return regex.test(address);  
+}
+function branchTest(branch)
+{
+    let regex = /^[a-zA-Z ]{2,30}$/;
+    return regex.test(branch);  
+}
+function businessnameTest(business)
+{
+    let regex = /^[a-zA-Z ]{2,30}$/;
+    return regex.test(business);  
+}
+function amountTest(amount)
+{
+    let regex = /^[0-9]{1,10}$/;
+    return regex.test(amount);  
+}
+function loantenureTest(loan)
+{
+    let regex = /^[0-9]{1,2}$/;
+    return regex.test(loan);  
+}
